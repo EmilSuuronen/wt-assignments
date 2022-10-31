e4dc00854650e23745e2da1e166777b9e7eb0eab
Emil <emilsuuronen@gmail.com>
2022-11-01 00:25:59 +0200

    Revert "added server 2 and a pug template"
    This reverts commit 0b80dbb81534fcac2c76a02e8eec4859b66c9368.

--- week-1/script.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const _ = require('lodash');
-
-console.log('This is JavaScript');
-
-let output = 'Foo Bar';
-console.log(output);
-output = _.camelCase(output);
-console.log(output);
