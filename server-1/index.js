1cedd37f9f4bfbdbddb8b6682fd1d56d918a4462
Emil <emilsuuronen@gmail.com>
2022-10-31 23:19:49 +0200

    added server 2 and pug template

---
+++ server-1/index.js
@@ -0,0 +1,24 @@
+'use strict';
+console.log('server 1 started');
+
+const express = require('express');
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+
+app.get("/catinfo", (req, res) => {
+    const cat = {
+        name: "Frank",
+        birthdate: "2010-12-25",
+        weight: 5,
+    };
+    res.json(cat);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+
+
