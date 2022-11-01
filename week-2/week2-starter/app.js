3231afe816db77cce4a38a158c1f490d338bd703
Emil <emilsuuronen@gmail.com>
2022-11-01 14:48:27 +0200

    added crud methods

--- week-2/week2-starter/app.js
+++ week-2/week2-starter/app.js
@@ -7,4 +7,28 @@ app.get('/cat', (req, res) => {
   res.send('From this endpoint you can get cats.')
 });
 
+app.get('/cat/:catId', (req, res) => {
+  res.send('From this endpoint you can get cats with id of ' + req.params.catId)
+});
+
+app.post('/cat', (req, res) => {
+  console.log(req)
+  res.send('From this endpoint you can add cats.')
+});
+
+app.put('/cat', (req, res) => {
+  console.log(req)
+  res.send('From this endpoint you can edit cats.')
+});
+
+app.delete('/cat', (req, res) => {
+  console.log(req)
+  res.send('From this endpoint you can delete cats.')
+});
+
+app.get('/users', (req, res) => {
+  res.send('From here you can view users.')
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
